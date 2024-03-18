8d1b639c5dda1b8aefd7909cfd58b2b3e997678a
CoffeeB <your.email@example.com>
2024-03-18 12:44:42 +0100

    RRF-20 modified the padding in the profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -34,7 +34,7 @@ export default function Profile({ targetRef }) {
         {activeStep === 'edit' && <ProfileEdit handleStepChange={handleStepChange} />}
         {(activeStep === 'details' || activeStep === 'settings') && 
         (<div className="row pt-lg-5 mt-lg-3 pt-sm-0 mt-sm-0 overflow-y-sm-hidden pb-0 vh-lg-100">
-            <div className="card border-0 border-grey rounded-0 col-lg-4 p-0 pe-0 border-end h-lg-100 h-md-75 ps-5 pt-3">
+            <div className="card border-0 border-grey rounded-0 col-lg-4 p-0 pe-0 border-end h-lg-100 h-md-75 ps-lg-3 pt-lg-3 pt-sm-2">
                 <div className="card-common overflow-y-scroll mh-100 h-100">
                     <div className="card-body pt-lg-5 pt-sm-2"   ref={targetRef}>
                         <div className="d-flex flex-column justify-content-between">
@@ -77,7 +77,7 @@ export default function Profile({ targetRef }) {
                     </div>
                 </div>
             </div>
-            <div className="card border-0 rounded-0 col-lg-8 p-0 ps-5 pt-3  overflow-x-hidden h-lg-100">
+            <div className="card border-0 rounded-0 col-lg-8 p-0 px-2 pt-3  overflow-x-hidden h-lg-100">
                 {activeStep === 'details' && <ProfileDetails />}
                 {activeStep === 'settings' && <ProfileSettings />}
             </div>
